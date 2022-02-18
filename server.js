88ced4eef0b380d89164037bf186faec6beb9e3f
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2022-02-18 06:20:31 +0300

    added a post event

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
@@ -21,11 +24,26 @@ const mockUserData=[
         })
     })
 
-
-
-
-
-
+    app.post('/login',function(req,res){
+        const username=req.body.username;
+        const password=req.body.password;
+    
+        const mockUsername="billyTheKid";
+        const mockPassword="superSecret";
+    
+        if (username===mockUsername && password===mockPassword){
+             res.json({
+                  success: true,
+                  message: 'password and username match!',
+                  token: 'encrypted token goes here'
+             })
+        } else {
+             res.json({
+                  success: false,
+                  message: 'password and username do not match'
+             })
+        }
+   })
 
 
 
